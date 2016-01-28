d98f62bc100da72693d4592ec8640cd4f3c66bdc
Medetich <kassymov.m.h@gmail.com>
2016-01-28 15:44:20 +0600

    prime numbers 1

--- Primenumber1/Primenumber1/Program.cs
+++ Primenumber1/Primenumber1/Program.cs
@@ -10,6 +10,20 @@ namespace Primenumber1
     {
         static void Main(string[] args)
         {
+            string[] numbers = Console.ReadLine().Split();
+            foreach (string s in numbers)
+            {
+                int a = int.Parse(s);
+                bool p = false;
+                for (int i = 2; i * i <= a; i++)
+                {
+                    if (a % i == 0)
+                        a = true;
+                }
+                if (p == false)
+                    Console.WriteLine(a);
+            }
+            Console.ReadKey();
         }
     }
 }
