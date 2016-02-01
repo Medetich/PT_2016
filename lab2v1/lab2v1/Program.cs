2a655e4eccd43190ad1544e7c4966f39460522c1
Medetich <kassymov.m.h@gmail.com>
2016-02-01 23:43:32 +0600

    theory file and

---
+++ lab2v1/lab2v1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2v1
+{
+    class lab2v1
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6];
+            FileStream fsi = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            FileStream fso = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader rs = new StreamReader(fsi);
+            StreamWriter sw = new StreamWriter(fso);
+            string[] massive = rs.ReadLine().Split(' ');
+            for (int i = 0; i < massive.Length; i++)
+                array[i] = int.Parse(massive[i]);
+            int k = 0;
+            int min = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 1; j < min; j++)
+                {
+                    if (array[i] % j == 0)
+                    {
+                        k++;
+                    }
+                }
+                if (k == 2 && array[i] < min)
+                {
+                    min = array[i];
+                }
+                k = 0;
+            }
+            sw.WriteLine("Minumum number: " + min);
+            sw.Close();
+            rs.Close();
+        }
+    }
+}
