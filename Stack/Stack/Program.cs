fdde11f4683f247389e67667f9cc4b73c2bee080
Medetich <kassymov.m.h@gmail.com>
2016-02-06 23:59:22 +0600

    lab 2 stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void LookIn(string path)
+        {
+            Stack<string> drs = new Stack<string>();
+            drs.Push(path);
+            while (drs.Count > 0)
+            {
+                DirectoryInfo current = new DirectoryInfo(drs.Pop());
+                DirectoryInfo[] subdrs = current.GetDirectories();
+                FileInfo[] files = current.GetFiles();
+                Console.WriteLine(current);
+                foreach (FileInfo file in files)
+                {
+                    Console.WriteLine(file);
+                }
+                foreach (DirectoryInfo s in subdrs)
+                {
+                    drs.Push(s.FullName);
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            LookIn(@"C:\WORK\far 2.0\");
+            Console.ReadKey();
+        }
+    }
+}
